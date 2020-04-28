aa1814f5cf1a7095aa8bff7b1e0ac9e85ac34cf6
Mrsherlock1996 <37865808+Mrsherlock1996@users.noreply.github.com>
2020-04-28 20:18:39 +0800

    略加备注且测试成功

--- FaceRecogniziton.cpp
+++ FaceRecogniziton.cpp
@@ -12,14 +12,6 @@ string haarFaceDataPath = "C:/opencv/build/etc/haarcascades/haarcascade_frontalf
 
 int main(int argc, char** argv)
 {
-	Mat testSample2 = cv::imread("C:/Users/xpp19/Desktop/test.jpg");
-	if (testSample2.empty()) {
-		cout << "no image..." << endl;
-		return -1;
-	}
-	namedWindow("testImage2", WINDOW_AUTOSIZE);
-	imshow("testImage2", testSample2);
-	cv::waitKey(60);
 	string fileName = "C:/Users/xpp19/Documents/test1228.txt";  //这里是我的脸的数据
 	ifstream file(fileName.c_str(), ifstream::in);
 	if (!file) {
@@ -45,38 +37,32 @@ int main(int argc, char** argv)
 			cout << "Convert Labels = " << temp << endl;
 		}
 	}
-	//test image
-	Mat testSample1 = images[1];
-	namedWindow("testImage1", WINDOW_AUTOSIZE);
-	imshow("testImage1", testSample1);
-	cv::waitKey(60);
+	//看一下样本的宽高
 	int height = images[0].rows;
 	int width = images[0].cols;
 	cout << "imageHeight= " << height << endl;
 	cout << "imageWidth= " << width << endl;
+	//简单的交叉验证, 取前S-1个样本, 保留最后一个样本作为test set来检测model预测能力
 	Mat testSample = images[int(images.size()) - 1];
 	int testLabel = labels[labels.size() - 1];
 	cout << "testLabel=" << testLabel << endl;
 	namedWindow("testImage", WINDOW_AUTOSIZE);
 	imshow("testImage", testSample);
 	cv::waitKey(60);
-	//stringstream convertLabels;
-	//string tempString;
-	//convertLabels << testLabel;
-	//convertLabels >> tempString;
+	//弹出test 样本
 	images.pop_back();
 	labels.pop_back();
 	//train it
 	Ptr<BasicFaceRecognizer> model = EigenFaceRecognizer::create();
 	model->train(images, labels);
 	cout << "training OK" << endl;
-
-	//recognition face
-	/*int predictedLabel =*/
-	//cout << "predictedLabel = " << model->predict(testSample) << endl;
-	//create A cascadeClaasifier
+	//看一下预测结果
+	cout << "predictedLabel = " << model->predict(testSample) << endl;
+	//create haarCascadeClaasifier
 	CascadeClassifier faceDetector;
 	faceDetector.load(haarFaceDataPath);
+
+	//注意级联分类器是用来识别是不是人脸的, 而训练的model是用来确定是不是你
 
 	//Open camera
 	VideoCapture capture(0);
@@ -91,28 +77,39 @@ int main(int argc, char** argv)
 	Mat dst;
 	cout << "进入识别区" << endl;
 	int faceNumber = 0;
-	while (capture.read(frame)) {
+	while (capture.read(frame)) { 
+/* 这里的逻辑是: 先读取一帧图片, 
+	然后利用haar级联分类器对该帧进行识别人脸,
+	如果没有人脸, 就不必预测是谁
+	直接等待10ms然后再读一帧, 等待时间可以修改*/
 		flip(frame, frame, 1);
 		faceDetector.detectMultiScale(frame, faces, 1.1, 3, 0, Size(50, 50), Size(800, 800));
 		for (int i = 0; i < faces.size(); i++) {
+		/* 这里即确定了是人脸, 但不知道是谁
+		因此需要在这里进行特征脸识别
+		即用训练好的model对该帧进行判别分类
+		*/
 			Mat roi = frame(faces[i]);    //提取感兴趣区域, area of interesting
 			//每一个face[i]都是一个Rect类对象
 			cout << "已提取感兴趣area" << endl;
-			cvtColor(roi, dst, COLOR_BGR2GRAY);
+			cvtColor(roi, dst, COLOR_BGR2GRAY); //用的EigenRec需要gray images
 			resize(dst, dst, testSample.size());
 			int label = 0;
 			stringstream tempStream;
-			tempStream << model->predict(dst);
-			tempStream >> label;
-			cout << "predict label = " << label << endl; //预测我的脸
-			//估计predict返回值给int类型时溢出了
-			rectangle(frame, faces[i], Scalar(0, 0, 255), 2, 8, 0);
+			tempStream << model->predict(dst);  //进行预测
+			tempStream >> label;                            //stringstream流来类型转换
+			cout << "predict label = " << label << endl; //输出预测结果
+			rectangle(frame, faces[i], Scalar(0, 0, 255), 2, 8, 0);  //绘制矩形框
+			/*显然这个解决方案并没有完全实现实时检测人脸并识别身份,
+			这是因为绘制矩形框的时机选择了在predict之后
+			可以思考绘制两个矩形框,一个识别人脸,一个辨识身份*/
 			if (label == 1228) {
 				stringstream convert;
 				convert << label;
 				string temp;
 				convert >> temp;
-				putText(frame, string(temp), faces[i].tl(), FONT_HERSHEY_PLAIN, 1.0, Scalar(0, 0, 255), 2, 8);
+				//把预测结果放到矩形框上
+				putText(frame, string(temp), faces[i].tl(), FONT_HERSHEY_PLAIN, 1.0, Scalar(0, 0, 255), 2, 8); 
 				imwrite(format("C:/Users/xpp19/Documents/Myfaces/%d.jpg", faceNumber++), frame);
 				cout << "已经写入第" << faceNumber << "个图片" << endl;
 			}
@@ -123,7 +120,7 @@ int main(int argc, char** argv)
 
 		}
 		cv::imshow(faceRecognitionWindow, frame);
-		cv::waitKey(10);
+		cv::waitKey(10000);
 		char c = cv::waitKey(10);
 		//按键q退出循环
 		if (c == 27) {
